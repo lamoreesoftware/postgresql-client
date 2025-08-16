410839a1fa1097e73d7cc22367e522328d9415e2
Joseph Lamoree <joseph@lamoree.com>
2025-08-16 15:48:47 -0700

    Updating for Debian Trixie with PostgreSQL 17.

--- build-and-push.sh
+++ build-and-push.sh
@@ -4,14 +4,12 @@ set -e
 
 registry="docker.io/lamoreesoftware"
 repository="postgresql-client"
-postgresql_major_version="14"
-postgresql_minor_version="12"
-tag="${1:-${postgresql_major_version}.${postgresql_minor_version}}"
 builder="local"
+dockerfile="Dockerfile.awscli"
+tag="${1:-$(date '+%Y%m%d01')}"
 
 docker buildx inspect "${builder}" > /dev/null 2>&1 || \
   docker buildx create --name "${builder}" --node local --platform linux/arm64,linux/amd64
 
 docker buildx build --builder "${builder}" --platform=linux/amd64,linux/arm64 --push \
-  --build-arg "POSTGRESQL_MAJOR_VERSION=${postgresql_major_version}" --build-arg "POSTGRESQL_MINOR_VERSION=${postgresql_minor_version}" \
-  --tag "${registry}/${repository}:${tag}" --file Dockerfile .
+  --tag "${registry}/${repository}:${tag}" --file "$dockerfile" .
